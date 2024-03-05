8db89a5881a431845850e6674ff9da92da3502b8
nicky89ck <nicollyrodrigues0220@gmail.com>
2024-03-05 14:32:52 -0300

    Update contador.java

--- Java Basico/desafioControleDeFluxo/ContadorJava.java
+++ Java Basico/desafioControleDeFluxo/ContadorJava.java
@@ -6,38 +6,41 @@ public class ContadorJava {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = input.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = input.nextInt();
+		int parametroDois;
 		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
+		//iniciando o looping para o usuário que informou o segundo parametro menor poder inserir novo dado correto
+		do {
+			System.out.println("Digite o segundo parâmetro:");
+			parametroDois = input.nextInt();
+			
+			try {
+				//chamando o método cotendo a lógica de contagem
+				contar(parametroUm, parametroDois);
+			}catch(ParametrosInvalidosException e) {
+				//Imprimir a mensagem:O segundo parâmetro deve ser maior que o primeiro
+				System.out.println(e.getMessage());
+			}
+		}while(parametroDois <= parametroUm);
 		
-		}catch (ParametrosInvalidosException e) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-			System.out.println(e.getMessage());
-		}
 		input.close();
 	}
-
-	private static void contar(int parametroUm, int parametroDois)throws ParametrosInvalidosException  {
-		// validar se parametro um é maior do que parametro dois
-		
-		if(parametroUm> parametroDois) {
-			throw new ParametrosInvalidosException("O segundo número deve ser maior do que o primeiro.");
+	
+	//metodo para realizar a contagem
+	private static void contar(int parametroUm, int parametroDois)throws ParametrosInvalidosException{
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parametro deve ser maior do que o primeiro.");
 		}
 		
-		int contagem = parametroDois - parametroUm;
-		
-		// Realizar o for para imprimir os números com base na variável contagem
-		for(int i = 1; i<=contagem; i++) {
-			 System.out.println("Imprimindo o número " + i);
+		//logica da contagem
+		int contagem  = parametroDois - parametroUm;
+		//Realizar o for pra impimir os numeros com base na variável contagem
+		for (int i = 1; i<= contagem; i++) {
+			System.out.println("imprimindo o número " + i);
 		}
-		
 	}
 	
 }
-
-
